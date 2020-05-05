8046f10093092b2fdf6559a252197040ac6a2d3a
Tonika Valderrama <43514676+tokich6@users.noreply.github.com>
2020-05-04 21:30:39 -0400

    Update mario.py

--- mario.py
+++ mario.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 # ask until you get a number between 1 and 8
 while True:
     height = get_int("Height: ")
-    if height > 0 and height < 9:
+    if height in range(1,9):
         break
 # logic for printing the hashes
 for i in range(height):
