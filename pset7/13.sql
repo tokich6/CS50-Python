5b7cda0fdf2019bb0e7fadca550fae6a464649fa
Tonika Valderrama <43514676+tokich6@users.noreply.github.com>
2020-05-10 14:08:03 -0400

    Create 13.sql

---
+++ pset7/13.sql
@@ -0,0 +1,11 @@
+SELECT distinct(name) FROM people
+JOIN stars
+ON people.id = stars.person_id
+JOIN movies
+ON stars.movie_id = movies.id
+WHERE movies.title IN (SELECT distinct(movies.title) from people
+JOIN stars
+ON people.id = stars.person_id
+JOIN movies
+ON stars.movie_id = movies.id
+WHERE people.name = 'Kevin Bacon' AND people.birth = 1958) AND people.name != 'Kevin Bacon';
